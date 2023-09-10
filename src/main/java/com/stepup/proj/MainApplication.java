d9a9fae09565533fcaeb55b833a229ca07c56be1
Alexander Shtaba <shtaba01@gmail.com>
2023-09-10 19:31:34 +0300

    Добавлен вызов DoSomething

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -5,6 +5,7 @@ public class MainApplication {
         System.out.println("----------");
         System.out.println("Hello World");
         System.out.println("1113311");
+        doSomething();
         System.out.println("---");
     }
 
